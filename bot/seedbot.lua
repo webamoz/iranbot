1553d8ad2e7709b83c4aedb4ceb42d1899497ef7
webamoz <mahnaz.9131@gmail.com>
2016-01-12 02:42:01 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {152205054},--Sudo users
+    sudo_users = {178275371},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
